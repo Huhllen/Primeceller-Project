b51d2ad42a8b557b3aa99ec2d93a6388c57e5c5b
Symon_Midsalipag <symonmidsalipag16@gmail.com>
2025-09-27 17:36:32 +0800

    frmLogin.vb

---
+++ frmLogin.vb
@@ -0,0 +1,59 @@
+Imports System.Data.SqlClient
+Imports System.Windows.Forms
+Public Class frmLogin
+    Private connectionString As String = "Data Source=localhost\SQLEXPRESS;Initial Catalog=PrimeCellerDB;Integrated Security=True;Encrypt=False;"
+
+    Private Sub frmLogin_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Me.ActiveControl = Nothing
+    End Sub
+
+    Private Sub btnBack_Click(sender As Object, e As EventArgs) Handles btnBack.Click
+        frmStart.Show()
+        Me.Hide()
+    End Sub
+
+    Private Sub btnlogin_Click(sender As Object, e As EventArgs) Handles btnlogin.Click
+        Dim username As String = txtUsername.Text.Trim()
+        Dim password As String = txtPassword.Text.Trim()
+
+        If AuthenticateUser(username, password) Then
+            MessageBox.Show("Login successful!")
+            frmFinanceDashBoard.Show()
+            Me.Hide()
+        Else
+            MessageBox.Show("Invalid username or password.")
+        End If
+    End Sub
+
+    Private Function AuthenticateUser(username As String, password As String) As Boolean
+        Dim isValid As Boolean = False
+        Using con As New SqlConnection(connectionString)
+            Dim query As String = "SELECT COUNT(*) FROM AdminAccounts WHERE Account_Username=@Username AND Account_Password=@Password"
+            Using cmd As New SqlCommand(query, con)
+                cmd.Parameters.AddWithValue("@Username", username)
+                cmd.Parameters.AddWithValue("@Password", password)
+                Try
+                    con.Open()
+                    Dim count As Integer = Convert.ToInt32(cmd.ExecuteScalar())
+                    If count > 0 Then
+                        isValid = True
+                    End If
+                Catch ex As Exception
+                    MessageBox.Show("Database error: " & ex.Message)
+                End Try
+            End Using
+        End Using
+        Return isValid
+    End Function
+
+    Private Sub txtUsername_KeyDown(sender As Object, e As KeyEventArgs) Handles txtUsername.KeyDown
+        If e.KeyCode = Keys.Enter Then
+            btnlogin.PerformClick()
+        End If
+    End Sub
+    Private Sub txtPassword_KeyDown(sender As Object, e As KeyEventArgs) Handles txtPassword.KeyDown
+        If e.KeyCode = Keys.Enter Then
+            btnlogin.PerformClick()
+        End If
+    End Sub
+End Class
