bc708e428acc28f333c0d84eabe45dbe072994cb
Symon_Midsalipag <symonmidsalipag16@gmail.com>
2025-09-27 17:35:38 +0800

    frmStart.vb

--- frmStart.vb
+++ frmStart.vb
@@ -1,1 +1,14 @@
-hehe
+Public Class frmStart
+    Private Sub frmStart_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Me.ActiveControl = Nothing
+    End Sub
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        frmLogin.Show()
+        Me.Hide()
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        frmLogin.Show()
+        Me.Hide()
+    End Sub
+End Class
